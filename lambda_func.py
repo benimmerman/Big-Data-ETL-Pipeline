d4d845d611807e1d2f28bfed7c6aa34d067ef4eb
Ben Immerman <113261578+benimmerman@users.noreply.github.com>
2023-06-06 17:05:39 -0400

    Update lambda_func.py

--- lambda_func.py
+++ lambda_func.py
@@ -7,7 +7,7 @@ from send_email import send_email
 s3_file_list = []
 
 s3_client=boto3.client('s3')
-for object in s3_client.list_objects_v2(Bucket='<s3 path to csv files to be checked>')['Contents']:
+for object in s3_client.list_objects_v2(Bucket='<s3 path to up to date csv files>')['Contents']:
     s3_file_list.append(object['Key'])
 
 datestr = time.strftime("%Y-%m-%d")
@@ -16,7 +16,7 @@ required_file_list = [f'calendar_{datestr}.csv', f'inventory_{datestr}.csv', f'p
 
 # scan S3 bucket
 if s3_file_list==required_file_list:
-    s3_file_url = ['s3://wcd-midterm-bimmerman/snowflake_db_input/' + a for a in s3_file_list]
+    s3_file_url = ['<s3 path to up to date csv files>' + a for a in s3_file_list]
     table_name = [a[:-15] for a in s3_file_list]   
 
     data = json.dumps({'conf':{a:b for a in table_name for b in s3_file_url}})
@@ -24,6 +24,6 @@ if s3_file_list==required_file_list:
     endpoint= 'http://<airflow_url_docker_ec2>/api/experimental/dags/midterm_dag/dag_runs'
 
     subprocess.run(['curl', '-X', 'POST', endpoint, '--insecure', '--data', data])
-    print('File are send to Airflow')
+    print('Files are sent to Airflow')
 else:
     send_email()
